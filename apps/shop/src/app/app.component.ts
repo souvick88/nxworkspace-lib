6773d9fcf2f451ce7bef3834f0267c1a6585a104
Vick <vick@vick.com>
2023-05-23 17:00:16 -0500

    setup

--- apps/shop/src/app/app.component.ts
+++ apps/shop/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { range } from '@foocompany/utils';
 
 @Component({
   selector: 'foocompany-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'shop';
+
+  ngOnInit(): void {
+    console.log(range(40, 45))
+  }
 }
